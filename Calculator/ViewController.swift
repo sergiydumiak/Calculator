4a71563afc4fc064850fcb8abd3e1eabfa4cf028
Pavlo Dumyak <pavlo.dumyak@kindgeek.com>
2021-08-24 12:27:52 +0300

    Initial Commit

---
+++ Calculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Pavlo Dumyak on 24.08.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
