e8a036bbce9e8046b3cede735023cae8387914f6
Pavlo Dumyak <pavlo.dumyak@kindgeek.com>
2021-08-24 13:14:42 +0300

    first commit

---
+++ Calculator/Common/Common.swift
@@ -0,0 +1,8 @@
+//
+//  Common.swift
+//  Calculator
+//
+//  Created by Pavlo Dumyak on 24.08.2021.
+//
+
+import Foundation
---
+++ Calculator/Helpers/Operations.swift
@@ -0,0 +1,8 @@
+//
+//  Operations.swift
+//  Calculator
+//
+//  Created by Pavlo Dumyak on 24.08.2021.
+//
+
+import Foundation
